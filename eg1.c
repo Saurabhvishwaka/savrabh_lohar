979cd770f3ab8467cfe221cf08723c7ad4c24360
Saurabhvishwaka <slohar.cs@gmail.com>
2025-02-02 20:03:50 +0530

    file added

---
+++ eg1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+x=26;
+y=4;
+z=x*y;
+printf("%d\n",z);
+return 0;
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+
+let m=document.getElementById("heading");
+console.log(m);
+window.alert("Hello");
+console.log("How are you");
+let s=document.getElementByClassName("heading1");
+console.log(s);
+
